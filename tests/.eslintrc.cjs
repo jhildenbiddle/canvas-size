09305a26ff91c800b86a172df1fc01fa19bbcdd0
John Hildenbiddle <john@hildenbiddle.com>
2024-02-08 11:14:40 -0600

    Remove test environment (karma, mocha, chai)

--- tests/.eslintrc.cjs
+++ tests/.eslintrc.cjs
@@ -2,19 +2,13 @@
 // http://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
 module.exports = {
     'env': {
-        'mocha': true
     },
     'plugins': [
-        'chai-expect',
-        'mocha'
     ],
     'parserOptions': {
         'ecmaVersion': 8,
     },
     'rules': {
-        'mocha/no-global-tests'   : ['error'],
-        'mocha/no-identical-title': ['error'],
-        'mocha/no-mocha-arrows'   : ['error'],
         'no-console'              : 'off',
     }
 };
